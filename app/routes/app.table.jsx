efb3ef90d5d1070fd50fad98271d1205dcf00de7
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-09-21 18:45:25 +0530

    added get data api

--- app/routes/app.table.jsx
+++ app/routes/app.table.jsx
@@ -1,9 +1,73 @@
-import { Page } from '@shopify/polaris'
+import { json } from '@remix-run/node'
+import { Page, Layout, Card, DataTable, Button, Modal } from '@shopify/polaris'
+import { useLoaderData, useFetcher } from '@remix-run/react'
+import { authenticate } from "../shopify.server"
 
+// inital popupulate data // get data from graphql
+export const loader = async ({ request }) => {
+    const { admin } = await authenticate.admin(request)
+    const response = await admin.graphql(`
+    {
+        products(first: 10) {
+          edges {
+            node {
+              id
+              title
+              description
+              vendor
+              variants(first: 1) {
+                edges {
+                    node {
+                        price
+                    }
+                }
+              }
+              images(first: 1) {
+                edges {
+                  node {
+                    src
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `)
+    const responseJson = await response.json()
+    const products = responseJson.data.products.edges.map((edge) => edge.node)
+    return json({ products })
+}
 
 export default function TablePage() {
-    return (
-        <Page title="Table Page">
-        </Page>
-    )
+  const { products } = useLoaderData()
+  const fetcher = useFetcher()
+
+  console.log('products', products)
+
+  const rows = products.map((product) => [
+    product.title,
+    product.description,
+    <img src={product.images?.[0]?.src} alt={product.title} width="50" key={product.id} />,
+    `$${product.variants[0]?.price || 0}`,
+    product.vendor,
+    <fetcher.Form method="post" key={product.id}>
+    </fetcher.Form>
+  ])
+
+  return (
+    <Page title="Table Page">
+      <Layout>
+        <Layout.Section>
+          <Card>
+            <DataTable
+              columnContentTypes={['text', 'text', 'text', 'numeric', 'text', 'text']}
+              headings={['Title', 'Description', 'Image', 'Price', 'Vendor', 'Actions']}
+              rows={rows}
+            />
+          </Card>
+        </Layout.Section>
+      </Layout>
+    </Page>
+  )
 }
