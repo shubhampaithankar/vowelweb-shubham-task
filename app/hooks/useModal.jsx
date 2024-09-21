b35b1094cc761cc962aba10a6aaf727b18151390
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-09-21 21:29:11 +0530

    modal changes

--- app/hooks/useModal.jsx
+++ app/hooks/useModal.jsx
@@ -1,69 +1,104 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import { Modal, TextField, FormLayout, Form } from "@shopify/polaris";
 
-export const useModal = (initialProduct, modalData) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [currentProduct, setCurrentProduct] = useState({
-    title: initialProduct.title || '',
-    description: initialProduct.description || '',
-    price: initialProduct.price || '',
-    vendor: initialProduct.vendor || ''
-  });
+export const useModal = (initialProduct) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [currentProduct, setCurrentProduct] = useState(initialProduct);
 
-  const onClose = () => {
-    setIsOpen(false)
-    setCurrentProduct({
-        title: '',
-        description: '',
-        price: '',
-        vendor: ''
-    })
-  }
+    const onOpen = useCallback((product) => {
+        setCurrentProduct(product || initialProduct);
+        setIsOpen(true);
+    }, [initialProduct]);
 
-  const handleChange = (field) => (value) => setCurrentProduct(prev => ({ ...prev, [field]: value }));
+    const onClose = useCallback(() => {
+        setCurrentProduct(initialProduct);
+        setIsOpen(false);
+    }, [initialProduct]);
 
-  const ModalDialog = () => {
-    return (
-      <Modal
-        open={isOpen}
-        onClose={onClose}
-        title={modalData.title}
-        primaryAction={modalData.primaryAction}
-      >
-        <Modal.Section>
-          <Form onSubmit={(e) => e.preventDefault()}>
-            <FormLayout>
-              <TextField
-                label="Title"
-                value={currentProduct.title}
-                onChange={handleChange('title')}
-              />
-              <TextField
-                label="Description"
-                value={currentProduct.description}
-                onChange={handleChange('description')}
-              />
-              <TextField
-                label="Price"
-                value={currentProduct.price}
-                onChange={handleChange('price')}
-                type="number"
-              />
-              <TextField
-                label="Vendor"
-                value={currentProduct.vendor}
-                onChange={handleChange('vendor')}
-              />
-            </FormLayout>
-          </Form>
-        </Modal.Section>
-      </Modal>
-    );
-  };
+    const handleChange = useCallback((field) => (value) => {
+        setCurrentProduct(prev => ({ ...prev, [field]: value }));
+    }, []);
 
-  return {
-    ModalDialog,
-    isOpen,
-    onClose,
-  };
+    const ModalDialog = useCallback(({ title, submit }) => {
+        const handleAction = () => {
+            try {
+                console.log(currentProduct)
+                switch (title) {
+                    case 'add':
+                        submit({
+                            _action: 'create',
+                            ...currentProduct
+                        }, { method: 'post' });
+                        break;
+                    case 'edit':
+                        submit({
+                            _action: 'edit',
+                            productId: currentProduct.id,
+                            ...currentProduct
+                        }, { method: 'put' });
+                        break;
+                    case 'delete':
+                        submit({
+                            _action: 'delete',
+                            productId: currentProduct.id,
+                        }, { method: 'delete' });
+                        break;
+                    default:
+                        break;
+                }
+            } catch (error) {
+                console.log(error);
+            } finally {
+                onClose();
+            }
+        };
+
+        return (
+            <Modal 
+                open={isOpen} 
+                onClose={onClose} 
+                title={title}
+                primaryAction={{ content: 'Submit', onAction: handleAction }}
+            >
+                <Modal.Section>
+                    <Form onSubmit={(e) => e.preventDefault()}>
+                        <FormLayout>
+                            <TextField
+                                label="Title"
+                                value={currentProduct.title}
+                                onChange={handleChange('title')}
+                                disabled={title === 'delete'}
+                            />
+                            <TextField
+                                label="Description"
+                                value={currentProduct.description}
+                                onChange={handleChange('description')}
+                                disabled={title === 'delete'}
+                            />
+                            <TextField
+                                label="Price"
+                                value={currentProduct.price}
+                                onChange={handleChange('price')}
+                                type="number"
+                                disabled={title === 'delete'}
+                            />
+                            <TextField
+                                label="Vendor"
+                                value={currentProduct.vendor}
+                                onChange={handleChange('vendor')}
+                                disabled={title === 'delete'}
+                            />
+                        </FormLayout>
+                    </Form>
+                </Modal.Section>
+            </Modal>
+        );
+    }, [isOpen, currentProduct, onClose, handleChange]);
+
+    return {
+        ModalDialog,
+        onOpen,
+        onClose,
+        currentProduct,
+    };
 };
